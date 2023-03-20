e7f4d73c79e5fca28d9799685a9d2adee6fa2d39
Harpreet Singh Gill <72593562+harpreetsgill@users.noreply.github.com>
2023-03-19 22:44:49 -0700

    mobile responsive styling

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -10,15 +10,18 @@ export default function Header() {
       <Link href="/" className={styles.logo}>
         Simple Dwelling
       </Link>
-      <div className={styles["episodes-submission"]}>
+      <div className={styles.nav + " " + styles["episodes-submission"]}>
         <Link href="/episodes" className={styles.episodes}>
           Episodes
         </Link>
-        <Link href="/submissions" className={styles.submission}>
+        <Link
+          href="/submissions"
+          className={styles.nav + " " + styles.submission}
+        >
           Submission
         </Link>
       </div>
-      <div className={styles.about}>
+      <div className={styles.nav + " " + styles.about}>
         <Link
           href="/about"
           className={router.asPath === "/about" ? styles.active : ""}
@@ -26,7 +29,7 @@ export default function Header() {
           About
         </Link>
       </div>
-      <div className={styles["advertise-contact"]}>
+      <div className={styles.nav + " " + styles["advertise-contact"]}>
         <Link
           href="/advertise"
           className={
@@ -37,7 +40,7 @@ export default function Header() {
         >
           Advertise
         </Link>
-        <Link href="/contact" className={styles.contact}>
+        <Link href="/contact" className={styles.nav + " " + styles.contact}>
           Contact
         </Link>
       </div>
